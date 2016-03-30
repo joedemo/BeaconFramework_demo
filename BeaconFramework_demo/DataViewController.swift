992eff5316fe848e911001c902c9a6c0be836ac5
JoeJoe <joejoe@joejoedemacbook-pro.local>
2016-03-30 18:09:21 +0800

    Initial Commit

---
+++ BeaconFramework_demo/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  BeaconFramework_demo
+//
+//  Created by JoeJoe on 2016/3/30.
+//  Copyright © 2016年 JoeJoe. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
