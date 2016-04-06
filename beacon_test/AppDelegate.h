2bd53833e266129c6be72c22fdbd6918aa022e71
JoeJoe <joejoe@joejoedemacbook-pro.local>
2016-04-06 11:12:07 +0800

    Initial Commit

---
+++ beacon_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  beacon_test
+//
+//  Created by JoeJoe on 2016/4/6.
+//  Copyright © 2016年 JoeJoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
