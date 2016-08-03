cf938cfa05257030c1165216d34f777627150545
JoeJoe <joejoe@joejoedemacbook-pro.local>
2016-08-03 17:34:07 +0800

    @fix ui efficacy issue, add device and sdk information

--- BeaconFramework_demo/AppDelegate.swift
+++ BeaconFramework_demo/AppDelegate.swift
@@ -77,21 +77,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate, IIIBeaconDetectionDelegat
             
             for item in detection.ActiveBeaconList! {
                     
-                    if !message_list.contains({$0.uuid == item.uuid}) {
+                    if !message_list.contains({$0.uuid == item.id}) {
                         
                         let value = _Message()
                         //建立推播內容物件
                         value.message = Notification.message()
-                        value.uuid = item.uuid
+                        value.uuid = item.id
                         
                         //取得Beacon對應推播內容
                         ////Production Environment ( If Test Environment, Please use get_push_message("52.69.184.56", ....) )
-                        notification.get_push_message_security("ideas.iiibeacon.net", major: Int(item.major!)!, minor: Int(item.minor!)!, key: "app key" ){ (completion) -> () in
+                        notification.get_push_message_security("ideas.iiibeacon.net", beacon_id: item.id!, key: "app key" ){ (completion) -> () in
                             
                             if(completion.Sucess){
                                 
                                 //資料回傳成功
-                                print("已取得 " + item.uuid! + " 資料; photoUrl: " + completion.msg.content!.coupons[0].photoUrl!)
+                                if completion.msg.content!.coupons.count > 0{
+                                 print("已取得 " + item.id! + " 資料; photoUrl: " + completion.msg.content!.coupons[0].photoUrl!)
+                                }
+                               
                             }
                         }
                         
