3ba6521820e0235580c804013f3047b92395eb15
JoeJoe <joejoedemo@gmail.com>
2017-04-17 18:02:40 +0800

    @ support Xcode8.3(swift3.1)

---
+++ xcode8.3 Framework/BeaconFramework.framework/Headers/BeaconFramework.h
@@ -0,0 +1,19 @@
+//
+//  BeaconFramework.h
+//  BeaconFramework
+//
+//  Created by JoeJoe on 2016/2/19.
+//  Copyright © 2016年 JoeJoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BeaconFramework.
+FOUNDATION_EXPORT double BeaconFrameworkVersionNumber;
+
+//! Project version string for BeaconFramework.
+FOUNDATION_EXPORT const unsigned char BeaconFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeaconFramework/PublicHeader.h>
+
+#import "libLBeaconKit.h"
